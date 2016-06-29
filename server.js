2fc76516ddfa45316759a79456ad606eaae49a95
Samuel Weisse <samuelweisse@Generals-MacBook-Air-5.local>
2016-06-29 08:55:54 -0700

    mongod connection

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 
 var port = process.env.PORT || 3000;
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/herokutest';
+
+var Person = mongoose.model('Person', {
+	name:String
+});
 
 app.get('/', function(req, res){
-	res.send('works');
+	Person.create({name:'matt'}, function(){
+		res.send('works');
+	})
+});
+
+mongoose.connect(mongoDBURI);
+mongoose.connection.once('open', function(){
+	console.log('connected to mongod');
 });
 
 app.listen(port, function(){
